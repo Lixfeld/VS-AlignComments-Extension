4a7699d12f313c71661034d066377f3255c065d6
Lixfeld <lixfeld-git@gmx.de>
2021-09-07 19:00:02 +0200

    Add new SelectedLines class

---
+++ src/AlignCommentsExtension/Classes/SelectedLines.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.Text;
+
+namespace AlignCommentsExtension.Classes
+{
+    public class SelectedLines
+    {
+        public int StartPosition { get; }
+        public int Length { get; }
+        public IEnumerable<string> Lines { get; }
+
+        public SelectedLines(ITextSnapshot textSnapshot, int startLineNo, int endLineNo)
+        {
+            StartPosition = textSnapshot.GetLineFromLineNumber(startLineNo).Start.Position;
+
+            List<string> selectedLines = new List<string>();
+            for (int i = startLineNo; i <= endLineNo; i++)
+            {
+                selectedLines.Add(textSnapshot.GetLineFromLineNumber(i).GetTextIncludingLineBreak());
+            }
+
+            // Length including line breaks
+            Length = selectedLines.Select(x => x.Count()).Sum();
+            Lines = selectedLines.Select(x => x.TrimEnd());
+        }
+    }
+}
